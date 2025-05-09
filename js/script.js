66d7de7c25612b04e4d6fa0e87a4c2b1c38ba2a0
rsacharias <rudolfsacharias@posteo.de>
2025-05-09 06:49:06 +0200

    feat: Add full calculator functionality
    To pass certain information about the state of the current calculation
    it was necessary to use additional global variables: 'clickedEquals',
    'clickedOperator' and 'isCleared'.

--- js/script.js
+++ js/script.js
@@ -15,6 +15,12 @@ function divide(a, b) {
 }
 
 function operate(a, b, op) {
+  a = parseFloat(a);
+  b = parseFloat(b);
+
+  console.log(`operate: a = ${a}, b = ${b}`);
+  if (isNaN(a) || isNaN(b)) return;
+
   switch (op) {
     case "+":
       return add(a, b);
@@ -27,33 +33,57 @@ function operate(a, b, op) {
   }
 }
 
-let firstOperand = 0;
-let secondOperand = undefined;
+let accumulator = "0";
+let nextOperand = "0";
 let operator = undefined;
+
+let clickedEquals = false;
+let clickedOperator = false;
+let isCleared = true;
 
 function reset() {
   document.querySelector(".display-operator").textContent = "";
   document.querySelector(".display-operand").textContent = "0";
 
-  firstOperand = 0;
-  secondOperand = undefined;
+  accumulator = "0";
+  nextOperand = "0";
   operator = undefined;
+
+  isCleared = true;
 }
 
-function updateOperand(operand, digit) {
-  return parseFloat(operand.toString() + digit);
+function clearDisplay(displayPortion) {
+  const display = document.querySelector(displayPortion);
+  display.textContent = "";
 }
 
 function updateDisplay(character) {
-  if (isNaN(parseInt(character))) {
-    document.querySelector(".display-operator").textContent = character;
-  } else {
-    const display = document.querySelector(".display-operand");
-    const operand = display.textContent;
+  let display;
+  switch (character) {
+    case ".":
+      display = document.querySelector(".display-operand");
+      if (!display.textContent.split("").includes(".")) {
+        display.textContent = display.textContent.trimEnd() + character;
+      }
+      break;
 
-    display.textContent =
-      operand === "0" ? character : operand.trimEnd() + character;
+    case "+":
+    case "-":
+    case "*":
+    case "/":
+      document.querySelector(".display-operator").textContent = character;
+      break;
+
+    // digit: 0 - 9
+    default:
+      display = document.querySelector(".display-operand");
+      const operand = display.textContent;
+
+      display.textContent =
+        operand === "0" ? character : operand.trimEnd() + character;
   }
+
+  return display;
 }
 
 const clearBtn = document.querySelector(".clear");
@@ -62,15 +92,58 @@ clearBtn.addEventListener("click", reset);
 const numbers = document.querySelectorAll(".number-container");
 numbers.forEach((number) => {
   number.addEventListener("click", (event) => {
-    if (event.target.classList.value === "number-container") return;
-    updateDisplay(event.target.textContent);
-    firstOperand = updateOperand(firstOperand, event.target.textContent);
-    console.log(firstOperand);
+    if (
+      event.target.classList.value === "number-container" ||
+      (event.target.classList.contains("decimal-point") &&
+        nextOperand.split("").includes("."))
+    )
+      return;
+
+    if (clickedEquals) {
+      reset();
+      clickedEquals = false;
+    }
+
+    if (clickedOperator) {
+      clearDisplay(".display-operand");
+      nextOperand = "0";
+      clickedOperator = false;
+    }
+
+    const digit = event.target.textContent;
+    updateDisplay(digit);
+    nextOperand = nextOperand === "0" ? digit : nextOperand + digit;
   });
 });
 
 const operators = document.querySelector(".operators");
 operators.addEventListener("click", (event) => {
   if (event.target.classList.value === "operators") return;
-  updateDisplay(event.target.textContent);
+
+  operator = event.target.textContent;
+  document.querySelector(".display-operator").textContent = operator;
+
+  if (clickedOperator) return;
+
+  if (!clickedEquals) {
+    accumulator = isCleared
+      ? nextOperand
+      : operate(accumulator, nextOperand, operator);
+  }
+
+  clickedOperator = true;
+  clickedEquals = false;
+  isCleared = false;
+});
+
+const equals = document.querySelector(".equals");
+equals.addEventListener("click", () => {
+  accumulator = isCleared
+    ? nextOperand
+    : operate(accumulator, nextOperand, operator);
+
+  document.querySelector(".display-operator").textContent = "";
+  document.querySelector(".display-operand").textContent = accumulator;
+
+  clickedEquals = true;
 });
